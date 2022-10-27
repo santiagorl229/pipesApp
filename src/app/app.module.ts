8df3d8e6396f5325151077caa7906c9c16ed9071
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2022-10-26 23:17:49 -0500

    Empezar Vid-17 Modulo-10(Timezone ...) Parte-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
+
+//cambiar el locale de la app
+import localEs from '@angular/common/locales/es-HN';
+import {registerLocaleData} from '@angular/common'
+registerLocaleData(localEs);
 
 @NgModule({
   declarations: [
@@ -16,7 +21,7 @@ import { VentasModule } from './ventas/ventas.module';
     AppRouterModule,
     VentasModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID,useValue:'es-HN'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
