ba75413bd25b6db1b7a807e5259cca55d395c729
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2023-01-09 20:39:26 -0500

    Empezar el video 23(SlicePipe)Modulo 10- Parte 3

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./no-comunes.component.css']
 })
 export class NoComunesComponent  {
+  //I18nSelect
   nombre: string ='Susana';
   genero: string ='femenino';
   invitacionMapa={
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
   }
+  //i18nPlural
+  clientes: string[]= ['Maria','Hernando', 'Eduardo','Roberto'];
+  clientesMapa ={
+    '=0': 'No tenemos a ningun cliente esperando ',
+    '=1': 'Tenemos a ningun cliente esperando ',
+    'other': 'Tenemos # clientes esperando'
+  }
+  cambiarCliente(){
+      this.nombre = 'Melissa';
+      this.genero = 'Fenenino';
+  }
+  borrarCliente(){
+    this.clientes.pop();
+  }
+
 }
