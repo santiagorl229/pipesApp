20a704471805166f176b345986cf4e12b92aa1bb
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2023-01-12 14:59:03 -0500

    Empezar el video 11(Primeng-Sortable Table) modulo 11- parte 3

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -7,6 +7,7 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
 import { VuelaPipe } from './pipes/vuela.pipe';
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 
 
 
@@ -17,7 +18,8 @@ import { VuelaPipe } from './pipes/vuela.pipe';
     BasicosComponent,
     OrdenarComponent,
     MayusculasPipe,
-    VuelaPipe
+    VuelaPipe,
+    OrdenarPipe
   ],
   exports:[
     NoComunesComponent,
